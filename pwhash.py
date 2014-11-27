f1c8eceb3af2848d20d906a7c57eb53d7f58ca0c
John Swanson <john.d.swanson@gmail.com>
2014-11-26 16:15:03 -0800

    bugfix

--- pwhash.py
+++ pwhash.py
@@ -5,7 +5,7 @@ import random
 salt_len = 12
 
 def passwd(passphrase=None, algorithm='sha1'):
-    h.hashlib.new(algorithm)
+    h = hashlib.new(algorithm)
     salt = ('%0' + str(salt_len) + 'x') % random.getrandbits(4 * salt_len)
     h.update(passphrase + salt)
-    return ':'.join(algorithm, salt, h.hexdigest())
+    return ':'.join((algorithm, salt, h.hexdigest()))
